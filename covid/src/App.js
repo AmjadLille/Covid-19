dafd2975b95f44a07002680ef9cf015b4acfc474
Amjad <sammanamjad86@gmail.com>
2020-05-01 19:21:43 +0200

    created all files

--- covid/src/App.js
+++ covid/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Cards, Chart, CountryPicker} from './components'
+import style from './App.module.css'
 
 class App extends React.Component{
     render(){
         return (
-            <div>
-                <h1>App</h1>
+            <div className={style.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart/>
             </div>
         )
     }
